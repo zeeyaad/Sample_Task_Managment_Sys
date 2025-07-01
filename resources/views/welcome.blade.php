76d2d57b55d9f3ad343c5b6766d15b1f9f18435d
Zed <zeyadalaa847@gmail.com>
2025-07-01 11:51:01 +0300

    1st Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', subject: app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sample Task Managment Sys</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <h1>
+            <a href="{{route('Home')}}">Zed Network</a>
+            </h1>
+            {{-- <a href="{{route('Ninjas.index')}}">All Ninjas</a>
+            <a href="{{route('Ninjas.create')}}">Add New Ninja</a> --}}
+        </nav>
+    </header>
+</body>
+</html>
